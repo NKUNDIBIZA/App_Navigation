aad5180f6ee5edd42b6174bdcd7ec2c5832223d9
Nkundibiza Ally <nkundibizaally@gmail.com>
2020-03-08 20:52:25 +0100

    Uwayezu Leader added some code in android

--- app/src/main/java/com/cyogere/nav_ctrl_1/MainActivity.kt
+++ app/src/main/java/com/cyogere/nav_ctrl_1/MainActivity.kt
@@ -2,11 +2,20 @@ package com.cyogere.nav_ctrl_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
-
+    var btn0:Button? = null
+    var btn1:Button? = null
+    var btn2:Button? = null
+    var btn3:Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn0 = findViewById(R.id.name_button0)
+        btn1 = findViewById(R.id.name_button0)
+        btn2 = findViewById(R.id.name_button0)
+        btn3 = findViewById(R.id.name_button0)
     }
 }
