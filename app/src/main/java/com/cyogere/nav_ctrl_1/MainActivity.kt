7586bd9c2121c48876f3c666bb9444b6570793b0
Nkundibiza Ally <nkundibizaally@gmail.com>
2020-03-09 14:14:16 +0100

    inflated Title fragment in main activity

--- app/src/main/java/com/cyogere/nav_ctrl_1/MainActivity.kt
+++ app/src/main/java/com/cyogere/nav_ctrl_1/MainActivity.kt
@@ -3,15 +3,16 @@ package com.cyogere.nav_ctrl_1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.databinding.DataBindingUtil
+import com.cyogere.nav_ctrl_1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        //setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 
-   
+
 }
