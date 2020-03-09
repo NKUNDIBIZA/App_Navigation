4a6f27cb50ec788e1a3bb8af6530c7b0601229f2
Nkundibiza Ally <nkundibizaally@gmail.com>
2020-03-09 23:26:40 +0100

    Inflated the gameFragment, added radioGroup to game layout

--- app/src/main/java/com/cyogere/nav_ctrl_1/GameFragment.kt
+++ app/src/main/java/com/cyogere/nav_ctrl_1/GameFragment.kt
@@ -2,25 +2,24 @@ package com.cyogere.nav_ctrl_1
 
 
 import android.os.Bundle
+import android.renderscript.ScriptGroup
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.cyogere.nav_ctrl_1.databinding.FragmentGameBinding
 
 /**
  * A simple [Fragment] subclass.
  */
 class GameFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return TextView(activity).apply {
-            setText(R.string.hello_blank_fragment)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+       val binding:FragmentGameBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_game, container,false)
+
+        return binding.root
         }
     }
-
-
-}
