2a432a7d710fdfa7c9a3187281995ec899ca6da5
Nkundibiza Ally <nkundibizaally@gmail.com>
2020-03-09 12:56:55 +0100

    add TitleFragment and removed unnecessary codes into MainActivity.kt

--- app/src/main/java/com/cyogere/nav_ctrl_1/MainActivity.kt
+++ app/src/main/java/com/cyogere/nav_ctrl_1/MainActivity.kt
@@ -5,20 +5,13 @@ import android.os.Bundle
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
-    var btn0:Button? = null
-    var btn1:Button? = null
-    var btn2:Button? = null
-    var btn3:Button? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn0 = findViewById(R.id.name_button0)
-        btn1 = findViewById(R.id.name_button1)
-        btn2 = findViewById(R.id.name_button2)
     }
 
-    private fun clickHandler() {
-        TODO("Not yet implemented")
-    }
+   
 }
