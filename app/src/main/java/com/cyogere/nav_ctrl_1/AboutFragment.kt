e0e26c916489f3b59fe0df492df2cc75bf35c89a
Nkundibiza Ally <nkundibizaally@gmail.com>
2020-03-10 22:39:51 +0100

    Programmatically Implemented the overFlow menu

--- app/src/main/java/com/cyogere/nav_ctrl_1/AboutFragment.kt
+++ app/src/main/java/com/cyogere/nav_ctrl_1/AboutFragment.kt
@@ -7,20 +7,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.cyogere.nav_ctrl_1.databinding.FragmentAboutBinding
 
-/**
- * A simple [Fragment] subclass.
- */
 class AboutFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return TextView(activity).apply {
-            setText(R.string.hello_blank_fragment)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        val binding: FragmentAboutBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_about, container, false)
+        return binding.root
         }
     }
-
-
-}
